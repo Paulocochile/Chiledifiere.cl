569f8a57065fbb40854b0e0a2b16ae947ab3beef
Paulocochile <63756183+Paulocochile@users.noreply.github.com>
2020-05-23 23:13:24 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+function myFunctionRicas() {
+  var x = document.getElementById("ImpuestosPersonasRicas");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    document.getElementById("plusP").innerHTML = "+";
+  } else {
+    x.style.display = "block";
+    document.getElementById("plusP").innerHTML = "-";
+  }
+
+}
+
+function myFunctionEmpresas() {
+  var x = document.getElementById("ImpuestosEmpresas");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    document.getElementById("plusE").innerHTML = "+";
+  } else {
+    x.style.display = "block";
+    document.getElementById("plusE").innerHTML = "-";
+  }
+
+}
+
+function myFunctionNormal() {
+  var x = document.getElementById("ImpuestosPersonasNormales");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    document.getElementById("plusN").innerHTML = "+";
+  } else {
+    x.style.display = "block";
+    document.getElementById("plusN").innerHTML = "-";
+  }
+
+}
